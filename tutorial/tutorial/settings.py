0fdaf2f974dab74863f3a970f855ac598706d0c0
Wamaitha <wamaithanyamu@gmail.com>
2022-02-04 15:56:37 +0300

    hyperlinking apis and relationships

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -50,7 +50,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 ROOT_URLCONF = 'tutorial.urls'
 
 TEMPLATES = [
